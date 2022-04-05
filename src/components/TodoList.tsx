9f30f5fe32a3685361f6cb46f66a40546ba0af8b
e1401 <hadrovic1@gmail.com>
2022-04-05 21:30:39 +0200

    Revert "Add signle todo edit"
    This reverts commit 806782c09196cb05f6a4339d0c55f4632b311a02.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,12 +12,11 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    // <div className="todos">
-    //   {todos.map((todo) => (
-    //     <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id} />
-    //   ))}{' '}
-    // </div>
-    <div className="container"></div>
+    <div className="todos">
+      {todos.map((todo) => (
+        <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id} />
+      ))}{' '}
+    </div>
   );
 };
 
